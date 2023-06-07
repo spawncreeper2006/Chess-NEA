11d6c698547e53c079dff6140bafde38601b02d7
spawncreeper2006 <97362498+spawncreeper2006@users.noreply.github.com>
2023-06-07 09:58:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,26 @@ import pygame
 from chess_engine import *
 
 
-pygame.init()
-
-BLACK_WOOD = (145, 60, 26)
-WHITE_WOOD = ( 245, 219, 135)
 BLACK = (0, 0, 0)
 GREEN = ( 0, 255, 0)
 RED = ( 255, 0, 0)
 FPS = 60
-
 WIDTH = 600
 HEIGHT = 600
-
 SQUARE_SIDE = WIDTH / 8
+
+
+def screen_to_chess_coords(coords:tuple) -> tuple:
+    coords = coords[0] // SQUARE_SIDE, coords[1] // SQUARE_SIDE
+    coords = coords[0] + 1, 7 - coords[1] + 1
+    coords = int(coords[0]), int(coords[1])
+    return coords
+
+pygame.init()
+
+
+
+
 
 FONT = pygame.font.SysFont(None, 24)
 
@@ -36,10 +43,11 @@ while running:
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
              pos = pygame.mouse.get_pos()
-             pos = pos[0] // SQUARE_SIDE, pos[1] // SQUARE_SIDE
-             coords = pos[0] + 1, 7 - pos[1] + 1
-             coords = int(coords[0]), int(coords[1])
+             coords = screen_to_chess_coords(pos)
+
+
              grid.coords(coords).clicked()
+
  
     screen.fill(WHITE_WOOD)
 
@@ -48,14 +56,19 @@ while running:
         x = n % 8
 
         coords = x*SQUARE_SIDE, y*SQUARE_SIDE
-        color = WHITE_WOOD if (x + y) % 2 == 0 else BLACK_WOOD
+        chess_coords = screen_to_chess_coords(coords)
+        
+        #color = WHITE_WOOD if (x + y) % 2 == 0 else BLACK_WOOD
+        square = grid.coords(chess_coords)
+        
 
-        pygame.draw.rect(screen, color, [coords[0], coords[1], SQUARE_SIDE, SQUARE_SIDE],0)
-        square = grid.coords((x+1, 8-y))
+        pygame.draw.rect(screen, square.color, [coords[0], coords[1], SQUARE_SIDE, SQUARE_SIDE],0)
+        
 
 
         if square.contains_piece:
             #print (coords, x, y, square)
+            
 
             screen.blit(FONT.render(square.piece.piece_identifier, True, BLACK), coords)
 
